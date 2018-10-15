b2e70a4778f3b3755194ba3fa2cdf3987654891d
junaid <mjunaid2017@namal.edu.pk>
2018-10-15 15:58:12 +0500

    starting

---
+++ task1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+class TollBooth
+{
+public:
+TollBooth()
+{
+number_of_cars=0;
+amount_of_money=0;
+}
+double payingCar()
+{
+}
+double nopaycar()
+{
+}
+private:
+unsigned int number_of_cars;
+double amount_of_money;
+};
+int main()
+{
+TollBooth cars;
+}
+
